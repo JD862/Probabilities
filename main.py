91e84e5a28a1f192eecefce7c02232186fc4cdac
JD862 <jaydenduong1202@gmail.com>
2026-05-06 21:06:05 -0700

    Cleanup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
+#import random # Not used yet
 import streamlit as st
 
 
-def roll_prob(sum):
+def roll_prob(sum): # Totals all possible ways to get a sum
 
     possibles = 0
     for dice_a in range (1, 7):
@@ -10,22 +10,22 @@ def roll_prob(sum):
             if dice_a + dice_b == sum:
                 possibles += 1
 
-    return possibles / 36 * 100
+    return possibles / 36 * 100 # In present
 
 def catan():
 
     if "num_inputs" not in st.session_state:
-        st.session_state.num_inputs = 3
+        st.session_state.num_inputs = 6 # Initial Tiles
 
     col1, col2 = st.columns(2)
 
     with col1:
         if st.button("(+) Add input"):
-            st.session_state.num_inputs += 1
+            st.session_state.num_inputs += 1 # Gain more tiles
 
     with col2:
         if st.button("Reset inputs"):
-            st.session_state.num_inputs = 3
+            st.session_state.num_inputs = 6 # New game
 
 
     with st.form ("Catan"):
@@ -34,31 +34,29 @@ def catan():
 
         tiles = []
         for i in range(st.session_state.num_inputs):
-            col1, col2 = st.columns(2)
-
+            col1, col2 = st.columns(2) # Divisions
 
             with col1:
-                options = ["Sheep", "Wheat", "Ore", "Brick", "Wood"]
-                resource = st.selectbox("Resource Type", options, key=f"res_{i}")
+                options = ["Sheep", "Wheat", "Ore", "Brick", "Wood"] # Choices
+                resource = st.selectbox("Resource Type", options, key=f"res_{i}") # Different keys
             with col2:
                 value = st.text_input(f"Win sum", key=f"win_{i}")
 
             tiles.append(resource + "/" + value)
 
-        wanted = st.multiselect("Wanted options", options, key="wanted_options")
+
+        wanted = st.multiselect("Wanted options", options, key="wanted_options") # What player wants
 
         submit = st.form_submit_button("Done")
 
         if submit:
-            st.write(tiles)
             prob = 0
 
             for i in range (len(tiles)):
-                if (tiles[i].split("/"))[0] in wanted:
+                if (tiles[i].split("/"))[0] in wanted: # Resource type 
                     prob += roll_prob(int((tiles[i].split("/"))[1]))
-                    st.write(prob)
 
-            st.write(f"Prob = {round(prob, 2)}%")
+            st.write(f"Prob = {round(prob, 2)}%") # Final
 
 
 def main():

4b7bd169633c6f930d2d37b4d05e6f54020b97ed
JD862 <jaydenduong1202@gmail.com>
2026-05-06 20:50:35 -0700

    Save values & calculated

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import streamlit as st
 def roll_prob(sum):
 
     possibles = 0
-    for dice_a in range (6):
-        for dice_b in range (6):
+    for dice_a in range (1, 7):
+        for dice_b in range (1, 7):
             if dice_a + dice_b == sum:
                 possibles += 1
 
@@ -15,17 +15,24 @@ def roll_prob(sum):
 def catan():
 
     if "num_inputs" not in st.session_state:
-        st.session_state.num_inputs = 2
+        st.session_state.num_inputs = 3
 
-    if st.button("(+) Add input"):
-        st.session_state.num_inputs += 1
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button("(+) Add input"):
+            st.session_state.num_inputs += 1
+
+    with col2:
+        if st.button("Reset inputs"):
+            st.session_state.num_inputs = 3
 
 
     with st.form ("Catan"):
         st.write("Catan Probabilities")
 
 
-        tiles = {}
+        tiles = []
         for i in range(st.session_state.num_inputs):
             col1, col2 = st.columns(2)
 
@@ -36,12 +43,22 @@ def catan():
             with col2:
                 value = st.text_input(f"Win sum", key=f"win_{i}")
 
-            tiles[resource] = value
+            tiles.append(resource + "/" + value)
+
+        wanted = st.multiselect("Wanted options", options, key="wanted_options")
 
         submit = st.form_submit_button("Done")
 
         if submit:
-            st.write("Prob = ")
+            st.write(tiles)
+            prob = 0
+
+            for i in range (len(tiles)):
+                if (tiles[i].split("/"))[0] in wanted:
+                    prob += roll_prob(int((tiles[i].split("/"))[1]))
+                    st.write(prob)
+
+            st.write(f"Prob = {round(prob, 2)}%")
 
 
 def main():
